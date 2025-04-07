45548e450b4e3b0ef37c9b5d99c380b1ab11ef3b
Abaljerind <abaljerinda@gmail.com>
2025-04-07 21:53:00 +0700

    Create ThemeContext for user toggle their theme
    Create variable ThemeContext
    Create function getInitialTheme to check and get theme of user's browser
    Create and export function ThemeProvider to toggle all elements's color inside this web based on theme

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useContext, useEffect } from "react";
+
+const ThemeContext = createContext();
+
+const getInitialTheme = () => {
+  if (typeof window !== "undefined") {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) return savedTheme === "dark";
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
+  return false;
+};
+
+// provider
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(getInitialTheme);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
