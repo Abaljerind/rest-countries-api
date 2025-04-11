5525b0af57ddfe0e77ae4244acfa676f86d43f9a
Abaljerind <abaljerinda@gmail.com>
2025-04-11 21:38:52 +0700

    Create component CountrySearch
    Use useState, react-icons and invoke Countries and FilterRegion
    Create 2 useState for search and select region
    Create input text to search country with value of event onChange
    Invoke component FilterRegion and send 2 props
    Invoke component Countries and send 2 props

---
+++ src/components/CountrySearch.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { PiMagnifyingGlassBold } from "react-icons/pi";
+import Countries from "./Countries";
+import FilterRegion from "./FilterRegion";
+
+// TODO: buat komponen untuk detail country
+
+const CountrySearch = () => {
+  const [searchCountry, setSearchCountry] = useState("");
+  const [selectedRegion, setSelectedRegion] = useState("Filter by Region");
+
+  return (
+    <div className="my-6">
+      <div className="bg-bg-secondary mx-4 rounded-md drop-shadow-sm">
+        <form className="flex items-center gap-4 py-2">
+          <PiMagnifyingGlassBold className="text-text-secondary ml-4 text-xl" />
+          <input
+            type="text"
+            onChange={(e) => setSearchCountry(e.target.value)}
+            className="text-text-primary mr-4 w-full py-1 text-sm outline-0 placeholder:text-sm"
+            placeholder="Search for a country..."
+          />
+        </form>
+      </div>
+
+      <FilterRegion
+        selectedRegion={selectedRegion}
+        setSelectedRegion={setSelectedRegion}
+      />
+
+      <Countries
+        searchCountry={searchCountry}
+        selectedRegion={selectedRegion}
+      />
+    </div>
+  );
+};
+
+export default CountrySearch;
