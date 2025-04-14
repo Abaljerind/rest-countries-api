70f885b47896dad5e409f90e55cf1af04e8acb86
AbalJerind <abaljerinda@gmail.com>
2025-04-14 09:55:08 +0700

    Add new path for detail page
    Invoke component _alpah3Code
    Add loader getAllCountries

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,11 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import { getAllCountries } from "../apis/loaders";
 import Layout from "../components/Layout";
+import _alpha3Code from "../components/_alpha3Code";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ path: "/", element: <Home />, loader: getAllCountries }],
+    children: [
+      { path: "/", element: <Home />, loader: getAllCountries },
+      {
+        path: "/detail/:alpha3Code",
+        element: <_alpha3Code />,
+        loader: getAllCountries,
+      },
+    ],
   },
 ]);
