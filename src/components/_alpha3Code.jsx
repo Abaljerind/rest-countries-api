059f22f058fda854709c729b33bc0ff61d64100d
Abaljerind <abaljerinda@gmail.com>
2025-04-14 16:34:05 +0700

    Add style for breakpoint xl for some elements
    Add style for Link, div, img, h2, p and h3

--- src/components/_alpha3Code.jsx
+++ src/components/_alpha3Code.jsx
@@ -44,81 +44,88 @@ const _alpha3Code = () => {
       <div className="p-6">
         <Link
           to={"/"}
-          className="bg-bg-secondary mt-2 mb-8 flex w-24 cursor-pointer items-center justify-evenly rounded-xs px-2 py-1 text-sm drop-shadow-md"
+          className="bg-bg-secondary mt-2 mb-8 flex w-24 cursor-pointer items-center justify-evenly rounded-xs px-2 py-1 text-sm drop-shadow-md xl:ml-8"
         >
           <FaArrowLeftLong />
           <p>Back</p>
         </Link>
 
-        <div className="flex justify-center">
-          <img
-            className="w-full md:h-64 md:w-[65%] md:object-cover"
-            src={detail?.flags.png}
-            alt={`${detail?.name} Flag`}
-          />
-        </div>
+        <div className="xl:grid xl:grid-cols-2 xl:items-center">
+          <div className="flex justify-center">
+            <img
+              className="w-full md:h-64 md:w-[65%] md:object-cover xl:h-80 xl:w-[80%]"
+              src={detail?.flags.png}
+              alt={`${detail?.name} Flag`}
+            />
+          </div>
 
-        <div className="md:mx-auto md:w-[65%]">
-          <div className="md:flex md:items-center md:justify-between">
-            <div>
-              <h2 className="mt-6 text-2xl font-extrabold">{detail?.name}</h2>
-              <div className="mt-4 flex flex-col gap-2">
-                <p className="text-sm font-bold">
-                  Native Name:{" "}
-                  <span className="font-light">{detail?.nativeName}</span>
+          <div className="md:mx-auto md:w-[65%] xl:w-[85%]">
+            <div className="md:flex md:items-center md:justify-between">
+              <div>
+                <h2 className="mt-6 text-2xl font-extrabold xl:text-3xl">
+                  {detail?.name}
+                </h2>
+                <div className="mt-4 flex flex-col gap-2">
+                  <p className="text-sm font-bold xl:text-base">
+                    Native Name:{" "}
+                    <span className="font-light">{detail?.nativeName}</span>
+                  </p>
+                  <p className="text-sm font-bold xl:text-base">
+                    Population:{" "}
+                    <span className="font-light">
+                      {detail?.population.toLocaleString()}
+                    </span>
+                  </p>
+                  <p className="text-sm font-bold xl:text-base">
+                    Region: <span className="font-light">{detail?.region}</span>
+                  </p>
+                  <p className="text-sm font-bold xl:text-base">
+                    Sub Region:{" "}
+                    <span className="font-light">{detail?.subregion}</span>
+                  </p>
+                  <p className="text-sm font-bold xl:text-base">
+                    Capital:{" "}
+                    <span className="font-light">{detail?.capital}</span>
+                  </p>
+                </div>
+              </div>
+
+              <div className="mt-6 flex flex-col gap-2">
+                <p className="text-sm font-bold xl:text-base">
+                  Top Level Domain:{" "}
+                  <span className="font-light">{detail?.topLevelDomain}</span>
                 </p>
-                <p className="text-sm font-bold">
-                  Population:{" "}
+                <p className="text-sm font-bold xl:text-base">
+                  Currencies:{" "}
                   <span className="font-light">
-                    {detail?.population.toLocaleString()}
+                    {detail?.currencies?.[0]?.name}
                   </span>
                 </p>
-                <p className="text-sm font-bold">
-                  Region: <span className="font-light">{detail?.region}</span>
-                </p>
-                <p className="text-sm font-bold">
-                  Sub Region:{" "}
-                  <span className="font-light">{detail?.subregion}</span>
-                </p>
-                <p className="text-sm font-bold">
-                  Capital: <span className="font-light">{detail?.capital}</span>
+                <p className="text-sm font-bold xl:text-base">
+                  Languages:{" "}
+                  <span className="font-light">
+                    {detail?.languages?.[0]?.nativeName}
+                  </span>
                 </p>
               </div>
             </div>
 
-            <div className="mt-6 flex flex-col gap-2">
-              <p className="text-sm font-bold">
-                Top Level Domain:{" "}
-                <span className="font-light">{detail?.topLevelDomain}</span>
-              </p>
-              <p className="text-sm font-bold">
-                Currencies:{" "}
-                <span className="font-light">
-                  {detail?.currencies?.[0]?.name}
-                </span>
-              </p>
-              <p className="text-sm font-bold">
-                Languages:{" "}
-                <span className="font-light">
-                  {detail?.languages?.[0]?.nativeName}
-                </span>
-              </p>
-            </div>
-          </div>
-
-          <div className="mt-6">
-            <h3 className="text-sm font-bold">Border Countries:</h3>
-            <div className="mt-4 grid grid-cols-3 gap-2">
-              {borderNames.map((border, index) => {
-                return (
-                  <p
-                    className="bg-bg-secondary flex items-center justify-center rounded-xs py-1 text-center text-xs font-light shadow-md md:px-2"
-                    key={index + 1}
-                  >
-                    {border}
-                  </p>
-                );
-              })}
+            <div className="mt-6">
+              <h3 className="text-sm font-bold xl:text-base">
+                Border Countries:
+              </h3>
+              <div className="mt-4 grid grid-cols-3 gap-2">
+                {borderNames.map((border, index) => {
+                  return (
+                    <p
+                      className="bg-bg-secondary flex items-center justify-center rounded-xs py-1 text-center text-xs font-light shadow-md md:px-2 xl:px-1 xl:py-2 xl:text-sm"
+                      key={index + 1}
+                    >
+                      {border}
+                    </p>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
