50a493da613567ed1fa33777a3aaa1a75a9c4cad
Abaljerind <abaljerinda@gmail.com>
2025-04-14 14:42:34 +0700

    Add style for breakpoint md for a few elements
    Add style for 3 divs and button

--- src/components/FilterRegion.jsx
+++ src/components/FilterRegion.jsx
@@ -16,18 +16,18 @@ const FilterRegion = ({ selectedRegion, setSelectedRegion }) => {
   }
 
   return (
-    <div className="relative mx-4 mt-6">
-      <div className="bg-bg-secondary w-[55%] cursor-pointer rounded-md p-4 drop-shadow-sm">
+    <div className="relative mx-4 mt-6 md:mt-0">
+      <div className="bg-bg-secondary w-[55%] cursor-pointer rounded-md p-4 drop-shadow-sm md:w-full">
         <button
           onClick={toggleDropdown}
-          className="flex w-full cursor-pointer items-center justify-between text-sm"
+          className="flex w-full cursor-pointer items-center justify-between text-sm md:mr-6 md:px-2"
         >
           {selectedRegion} <RiArrowDownSLine />
         </button>
       </div>
 
       {isOpen && (
-        <div className="absolute mt-2 w-[55%] drop-shadow-sm">
+        <div className="absolute mt-2 w-[55%] drop-shadow-sm md:w-full">
           <ul className="bg-bg-secondary cursor-pointer rounded-md px-4 py-2">
             {regions.map((region) => {
               return (
