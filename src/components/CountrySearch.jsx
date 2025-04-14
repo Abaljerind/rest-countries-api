f7c2e5136024669b4f7d4110bebc00ac9a74c2e6
Abaljerind <abaljerinda@gmail.com>
2025-04-14 14:41:28 +0700

    Add style for breakpoint md and wrap a few elements

--- src/components/CountrySearch.jsx
+++ src/components/CountrySearch.jsx
@@ -8,23 +8,27 @@ const CountrySearch = () => {
   const [selectedRegion, setSelectedRegion] = useState("Filter by Region");
 
   return (
-    <div className="my-6">
-      <div className="bg-bg-secondary mx-4 rounded-md drop-shadow-sm">
-        <form className="flex items-center gap-4 py-2">
-          <PiMagnifyingGlassBold className="text-text-secondary ml-4 text-xl" />
-          <input
-            type="text"
-            onChange={(e) => setSearchCountry(e.target.value)}
-            className="text-text-primary mr-4 w-full py-1 text-sm outline-0 placeholder:text-sm"
-            placeholder="Search for a country..."
-          />
-        </form>
-      </div>
+    <div className="my-6 md:px-2">
+      <div className="md:mx-9 md:grid md:max-w-full md:grid-cols-2 md:items-center">
+        <div className="bg-bg-secondary mx-4 rounded-md drop-shadow-sm md:w-3/4">
+          <form className="flex items-center gap-4 py-2">
+            <PiMagnifyingGlassBold className="text-text-secondary ml-4 text-xl" />
+            <input
+              type="text"
+              onChange={(e) => setSearchCountry(e.target.value)}
+              className="text-text-primary mr-4 w-full py-1 text-sm outline-0 placeholder:text-sm"
+              placeholder="Search for a country..."
+            />
+          </form>
+        </div>
 
-      <FilterRegion
-        selectedRegion={selectedRegion}
-        setSelectedRegion={setSelectedRegion}
-      />
+        <div className="md:flex md:justify-end">
+          <FilterRegion
+            selectedRegion={selectedRegion}
+            setSelectedRegion={setSelectedRegion}
+          />
+        </div>
+      </div>
 
       <Countries
         searchCountry={searchCountry}
