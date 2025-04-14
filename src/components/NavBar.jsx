79beb4670ad4b1da062a8dd1ec7b297779ab6d0a
Abaljerind <abaljerinda@gmail.com>
2025-04-14 14:44:49 +0700

    Add style for breakpoint md for a div

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./ThemeToggle";
 const NavBar = () => {
   return (
     <div className="bg-bg-input shadow">
-      <div className="flex items-center justify-between p-4">
+      <div className="flex items-center justify-between p-4 md:px-6">
         <div>
           <p className="text-text-primary font-extrabold">
             Where in the world?
