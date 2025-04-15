8aee315253b6e4f6e0ea3d5b63912172e8b8c69f
Abaljerind <abaljerinda@gmail.com>
2025-04-15 18:13:08 +0700

    Add style for breakpoint xl

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <div className="bg-bg-input shadow">
       <div className="flex items-center justify-between p-4 md:px-6 xl:mx-auto xl:w-[95%]">
         <div>
-          <p className="text-text-primary font-extrabold">
+          <p className="text-text-primary font-extrabold xl:text-2xl">
             Where in the world?
           </p>
         </div>
