f5e815cd7101f001f6fdfdd04916d3693872f158
Abaljerind <abaljerinda@gmail.com>
2025-06-13 17:01:17 +0700

    Feat(router): add errorElement using ErrorPage component

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,11 +3,13 @@ import Home from "../pages/Home";
 import { getAllCountries } from "../apis/loaders";
 import Layout from "../components/Layout";
 import _alpha3Code from "../components/_alpha3Code";
+import ErrorPage from "../components/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <Home />, loader: getAllCountries },
       {
