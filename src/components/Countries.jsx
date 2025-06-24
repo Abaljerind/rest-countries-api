ff1b8bf20f725e58a23c3a3168cf9db0acc83a02
Abaljerind <abaljerinda@gmail.com>
2025-06-24 18:12:49 +0700

    Refactor(Countries): update data access patterns to match API structure
    Replace country.name with country.name.common
    Replace country.alpha3Code with cca3
    Adjust object-fill to flag <img>
    Replace country?.capital to country?.capital[0]

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -7,7 +7,7 @@ const Countries = ({ searchCountry, selectedRegion }) => {
   const [allCountries, setAllCountries] = useState(countries);
 
   const deleteCountry = allCountries.filter((country) => {
-    if (country.name !== "Israel") {
+    if (country.name.common !== "Israel") {
       return country;
     }
   });
@@ -23,7 +23,7 @@ const Countries = ({ searchCountry, selectedRegion }) => {
       if (searchCountry === "") {
         return country;
       } else if (
-        country.name.toLowerCase().includes(searchCountry.toLowerCase())
+        country.name.common.toLowerCase().includes(searchCountry.toLowerCase())
       ) {
         return country;
       }
@@ -39,19 +39,21 @@ const Countries = ({ searchCountry, selectedRegion }) => {
       {displayCountries.map((country, index) => {
         return (
           <Link
-            to={`/detail/${country.alpha3Code}`}
+            to={`/detail/${country.cca3}`}
             key={index + 1}
             className="bg-bg-secondary mx-auto w-[70%] overflow-hidden rounded-md shadow-md transition-all duration-300 hover:scale-105 hover:shadow-2xl xl:w-[95%]"
           >
             <div>
               <img
-                src={country?.flag}
-                alt={country?.name}
-                className="h-40 w-full object-cover"
+                src={country?.flags.png}
+                alt={country?.flags.alt}
+                className="h-40 w-full object-fill"
               />
             </div>
             <div className="p-6">
-              <p className="mb-3 text-lg font-extrabold">{country?.name}</p>
+              <p className="mb-3 text-lg font-extrabold">
+                {country?.name.common}
+              </p>
               <p className="text-sm">
                 <span className="font-semibold">Population:</span>{" "}
                 <span className="font-light">
@@ -64,7 +66,7 @@ const Countries = ({ searchCountry, selectedRegion }) => {
               </p>
               <p className="text-sm">
                 <span className="font-semibold">Capital:</span>{" "}
-                <span className="font-light">{country?.capital}</span>
+                <span className="font-light">{country?.capital[0]}</span>
               </p>
             </div>
           </Link>
